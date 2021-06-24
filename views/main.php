989bb67bfc1bb72f52e51fae0e1d976000318450
PURGINGGG-1337 <67078617+PURGINGGG-1337@users.noreply.github.com>
2021-06-24 18:01:38 +0100

    Update main.php

--- views/main.php
+++ views/main.php
@@ -1,84 +1,24 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Lynx - Web</title>
-  <link href="/css/style.css" rel="stylesheet" type="text/css">
-</head>
+const app = require('express')(),
+path = require('path')
 
-<body>
-  <div class="container-fluid h-100">
-    <div class="row justify-content-center h-100">
-      <div class="col-md-4 col-xl-3 chat">
-        <div class="card2 mb-sm-3 mb-md-0 contacts_card">
-          <div class="card-header">
-            <div class="input-group">
-              <input type="text" placeholder="Search..." name="" class="form-control search">
-              <div class="input-group-prepend">
-                <span class="input-group-text search_btn"><i class="fas fa-search"></i></span>
-              </div>
-            </div>
-          </div>
-          <div class="card-body contacts_body">
-            <ui class="contacts">
-              <li class="active" id='users'>
-                <div class="d-flex bd-highlight">
-                  <div class="img_cont">
-                    <img src="https://i.pinimg.com/originals/7c/e8/d6/7ce8d6c44b056a3e9eb84d31139f4132.png"
-                      class="rounded-circle user_img">
-                    <span class="online_icon"></span>
-                  </div>
-                  <div class="user_info">
-                    <span>Lynxou</span>
-                    <p>🟢</p>
-                  </div>
-                </div>
-              </li>
-            </ui>
-          </div>
-          <div class="card-footer"></div>
-        </div>
-      </div>
-      <div class="col-md-8 col-xl-6 chat">
-        <div class="card">
-          <div class="card-header msg_head">
-            <div class="d-flex bd-highlight">
-            </div>
-            <span id="action_menu_btn"><i class="fas fa-ellipsis-v"></i></span>
-            <div class="action_menu">
-              <ul>
-                <li><i class="fas fa-user-circle"></i> Voir le profile</li>
-                <li><i class="fas fa-users"></i> Add to close friends</li>
-                <li><i class="fas fa-plus"></i> Ajouter au groupe</li>
-                <li><i class="fas fa-ban"></i> Bloquer</li>
-              </ul>
-            </div>
-          </div>
-          <div class="card-body msg_card_body">
-            <div class="d-flex justify-content-start mb-4">
-              <ul style="padding-left: 5px;" id="messages"></ul>
-            </div>
-          </div>
-          <div class="card-footer">
-            <div class="input-group">
-              <div class="input-group-append">
-                <span class="input-group-text attach_btn"><i class="fas fa-paperclip"></i></span>
-              </div>
-              <input name="" class="form-control type_msg" id="input" autocomplete="off"
-                placeholder="Type your message..." />
-              <div class="input-group-append">
-                <button style="border: none;outline-style: none;"
-                  class="fas fa-location-arrow input-group-text send_btn" id="send">Envoyer</button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</body>
-<script>
+app.get('/', (req, res)=> {
+    return res.status(203).json({ status: true, code: 203, message: "API ✔️-t"})
+})
+
+app.get('/home', (req, res)=> {
+    return res.send("<p>salut mec moi je suis sympa sauf que fils de pute arrête de me casser les couilles</p>")
+})
+
+let start = new Date()
+
+app.listen(process.env.PORT || 3000, (e)=> {
+    if(e) throw e
+    console.log(`[\x1b[36mServer\x1b[0m]: \x1b[32mstarted successfully at\x1b[0m [\x1b[36mlocalhost:${process.env.PORT || 3000}\x1b[0m] \x1b[32min\x1b[0m [\x1b[36m${new Date() - start}ms\x1b[0m]`)
+})
 
 
-</script>
-
-</html>
+require('./engine')(app, require('express'))
+require('glob').sync('src/utils/**/*.js').forEach(a=> {
+    //require(path.resolve(a))(app)
+    //console.log(path.resolve(a))
+})
